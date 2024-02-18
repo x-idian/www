4b433df743a02900bf70fcb1d95d044f40543071
Daniel Ng See Cheong <danielpnsc@gmail.com>
2024-02-18 20:50:58 +0200

    daisy ui

---
+++ src/routes/+page.svelte
@@ -0,0 +1,20 @@
+<div class="hero min-h-screen" style="background-image: url(https://daisyui.com/images/stock/photo-1507358522600-9f71e620c44e.jpg);">
+    <div class="hero-overlay bg-opacity-60"></div>
+    <div class="hero-content text-center text-neutral-content">
+        <div class="max-w-md">
+            <h1 class="mb-5 text-5xl font-bold">Hello there</h1>
+            <p class="mb-5">Provident cupiditate voluptatem et in. Quaerat fugiat ut assumenda excepturi exercitationem quasi. In deleniti eaque aut repudiandae et a id nisi.</p>
+            <button class="btn btn-primary">Get Started</button>
+        </div>
+    </div>
+</div>
+<div class="hero min-h-screen bg-base-200">
+    <div class="hero-content flex-col lg:flex-row">
+        <img src="https://daisyui.com/images/stock/photo-1635805737707-575885ab0820.jpg" class="max-w-sm rounded-lg shadow-2xl" />
+        <div>
+            <h1 class="text-5xl font-bold">Box Office News!</h1>
+            <p class="py-6">Provident cupiditate voluptatem et in. Quaerat fugiat ut assumenda excepturi exercitationem quasi. In deleniti eaque aut repudiandae et a id nisi.</p>
+            <button class="btn btn-primary">Get Started</button>
+        </div>
+    </div>
+</div>
---
+++ src/routes/about/+page.svelte
@@ -0,0 +1,63 @@
+<script>
+    import vision_img from '$lib/assets/about/vision.jpeg';
+</script>
+
+<section class="flex items-center xl:h-screen font-poppins">
+    <div class="justify-center flex-1 max-w-6xl py-4 mx-auto lg:py-6 md:px-6">
+        <div class="flex flex-wrap ">
+            <div class="w-full px-4 mb-10 lg:w-1/2 lg:mb-0">
+                <div class="relative lg:max-w-md">
+                    <img class="relative object-cover w-full rounded" alt="Our vision" src={vision_img} />
+                </div>
+            </div>
+            <div class="w-full px-6 mb-10 lg:w-1/2 lg:mb-0 ">
+                <div class="pl-4 mb-6 border-l-4 border-blue-500 ">
+                    <span class="text-sm uppercase">Who we are?</span>
+                    <h1 class="mt-2 text-3xl font-bold md:text-5xl">
+                        About Us
+                    </h1>
+                </div>
+                <div class="flex mb-4">
+                        <span
+                                class="flex items-center justify-center flex-shrink-0 w-12 h-12 mr-6 bg-blue-500 rounded dark:bg-blue-500 dark:text-gray-100 text-gray-50">
+                            <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor"
+                                 class="w-5 h-5 bi bi-file-text" viewBox="0 0 16 16">
+                                <path
+                                        d="M5 4a.5.5 0 0 0 0 1h6a.5.5 0 0 0 0-1H5zm-.5 2.5A.5.5 0 0 1 5 6h6a.5.5 0 0 1 0 1H5a.5.5 0 0 1-.5-.5zM5 8a.5.5 0 0 0 0 1h6a.5.5 0 0 0 0-1H5zm0 2a.5.5 0 0 0 0 1h3a.5.5 0 0 0 0-1H5z" />
+                                <path
+                                        d="M2 2a2 2 0 0 1 2-2h8a2 2 0 0 1 2 2v12a2 2 0 0 1-2 2H4a2 2 0 0 1-2-2V2zm10-1H4a1 1 0 0 0-1 1v12a1 1 0 0 0 1 1h8a1 1 0 0 0 1-1V2a1 1 0 0 0-1-1z" />
+                            </svg>
+                        </span>
+                    <div>
+                        <h2 class="mb-4 text-xl font-bold leading-tight md:text-2xl">
+                            Our vision
+                        </h2>
+                        <p class="text-base leading-loose">
+                            To transform the world by eliminating mundane work, liberating people to pursue richer and more fulfilling endeavors.
+                        </p>
+                    </div>
+                </div>
+                <div class="flex mb-4">
+                        <span
+                                class="flex items-center justify-center flex-shrink-0 w-12 h-12 mr-6 bg-blue-500 rounded dark:bg-blue-500 dark:text-gray-100 text-gray-50">
+                            <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor"
+                                 class="w-5 h-5 bi bi-file-text" viewBox="0 0 16 16">
+                                <path
+                                        d="M5 4a.5.5 0 0 0 0 1h6a.5.5 0 0 0 0-1H5zm-.5 2.5A.5.5 0 0 1 5 6h6a.5.5 0 0 1 0 1H5a.5.5 0 0 1-.5-.5zM5 8a.5.5 0 0 0 0 1h6a.5.5 0 0 0 0-1H5zm0 2a.5.5 0 0 0 0 1h3a.5.5 0 0 0 0-1H5z" />
+                                <path
+                                        d="M2 2a2 2 0 0 1 2-2h8a2 2 0 0 1 2 2v12a2 2 0 0 1-2 2H4a2 2 0 0 1-2-2V2zm10-1H4a1 1 0 0 0-1 1v12a1 1 0 0 0 1 1h8a1 1 0 0 0 1-1V2a1 1 0 0 0-1-1z" />
+                            </svg>
+                        </span>
+                    <div>
+                        <h2 class="mb-4 text-xl font-bold leading-tight md:text-2xl">
+                            Our mission
+                        </h2>
+                        <p class="text-base leading-loose">
+                            X-idian is dedicated to transforming the insurance landscape, eradicating routine burdens to allow for a focus on more strategic, satisfying, and customer-focused initiatives.
+                        </p>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+</section>
